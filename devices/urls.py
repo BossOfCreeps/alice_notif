afc1b27fcf065608fd3f961834f60ebd4297078d
Rachis Vsevolod <seva-ra4is@mail.ru>
2023-12-15 12:56:08 +0700

    AN-3: add notif test (#3)

--- devices/urls.py
+++ devices/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from devices.views import StatusView, DeviceListView
+from devices.views import StatusView, DeviceListView, DeviceDetailView
 
 urlpatterns = [
     path("", StatusView.as_view(), name="status"),
+    path("user/unlink", StatusView.as_view(), name="unlink"),
     path("user/devices", DeviceListView.as_view(), name="devices"),
+    path("user/devices/query", DeviceDetailView.as_view(), name="query"),
+    path("user/devices/action", StatusView.as_view(), name="action"),
 ]
--- devices/views.py
+++ devices/views.py
@@ -9,6 +9,9 @@ class StatusView(APIView):
     def get(self, request):  # noqa
         return Response()
 
+    def post(self, request):  # noqa
+        return Response()
+
 
 class DeviceListView(APIView):
     def get(self, request, *args, **kwargs):  # noqa
@@ -21,3 +24,56 @@ class DeviceListView(APIView):
                 },
             }
         )
+
+
+class DeviceDetailView(APIView):
+    def post(self, request, *args, **kwargs):  # noqa
+        devices = Device.objects.filter(id__in=[int(d["id"]) for d in request.data.get("devices", [])])
+        return Response(
+            {
+                "request_id": request.headers.get("X-Request-Id"),
+                "payload": {
+                    "devices": DeviceSerializer(Device.objects.all(), many=True).data,
+                    "error_code": "",
+                    "error_message": "",
+                },
+            }
+        )
+
+
+"""
+{
+    "request_id": "9f124dc4-c944-4256-88b2-b1b0cb778616",
+    "payload": {
+        "user_id": "None",
+        "devices": [
+            {
+                "id": "1",
+                "capabilities": [],
+                "properties": [
+                    {
+                        "id": 1,
+                        "name": "Влажность",
+                        "type": "devices.properties.float",
+                        "retrievable": true,
+                        "reportable": true,
+                        "parameters": {"instance": "humidity", "unit": "unit.percent"},
+                    }
+                ],
+                "device_info": {
+                    "id": 1,
+                    "manufacturer": "Рога и копыта",
+                    "model": "1",
+                    "hw_version": "1",
+                    "sw_version": "1",
+                },
+                "name": "увлажнитель",
+                "description": "Описание",
+                "type": "devices.types.humidifier",
+                "room": "Комната",
+                "custom_data": {},
+            }
+        ],
+    },
+}
+"""
